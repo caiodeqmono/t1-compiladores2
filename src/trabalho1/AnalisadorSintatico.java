1e141492096b26588a362bdf2b2e43093853b367
caiodeqmono <caiodeqmono@gmail.com>
2016-11-26 22:04:42 -0200

    Refatora T1ErrorListener para AnalisadorSintatico

---
+++ src/trabalho1/AnalisadorSintatico.java
@@ -0,0 +1,40 @@
+package trabalho1;
+
+import java.util.BitSet;
+import org.antlr.v4.runtime.BaseErrorListener;
+import org.antlr.v4.runtime.CommonToken;
+import org.antlr.v4.runtime.Parser;
+import org.antlr.v4.runtime.RecognitionException;
+import org.antlr.v4.runtime.Recognizer;
+import org.antlr.v4.runtime.atn.ATNConfigSet;
+import org.antlr.v4.runtime.dfa.DFA;
+
+public class AnalisadorSintatico extends BaseErrorListener {
+
+    saidaParser sp;
+
+    public AnalisadorSintatico(saidaParser sp) {
+        this.sp = sp;
+    }
+
+    @Override
+    public void syntaxError(Recognizer<?, ?> rcgnzr, Object o, int i, int i1, String string, RecognitionException re) {
+        if (!sp.isModificado()) { 
+            String token = ((CommonToken) o).getText();
+            token = token.replace("<EOF>", "EOF");
+            sp.println("Linha " + i + ": erro sintatico proximo a " + token );
+        }     
+    }
+
+    @Override
+    public void reportAmbiguity(Parser parser, DFA dfa, int i, int i1, boolean bln, BitSet bitset, ATNConfigSet atncs) {
+    }
+
+    @Override
+    public void reportAttemptingFullContext(Parser parser, DFA dfa, int i, int i1, BitSet bitset, ATNConfigSet atncs) {
+    }
+
+    @Override
+    public void reportContextSensitivity(Parser parser, DFA dfa, int i, int i1, int i2, ATNConfigSet atncs) {
+    }
+}
